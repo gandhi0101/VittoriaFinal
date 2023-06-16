09c2d23b1371ab33d4befc8096eaf77069b9be64
unknown <brayantoledo28@gmail.com>
2023-06-16 14:46:05 -0600

    Se mejoro la accesibilidad en home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,10 @@ import { HeaderComponent } from '../header/header.component';
 })
 
 export class HomeComponent implements OnInit{
+  
   url="https://my-json-server.typicode.com/juanMa281/api1/db";
   httpData: any;
   array:any[]=[];
-  homeaccs= new HeaderComponent
   
   
   constructor(private httpclient:HttpClient){
@@ -21,10 +21,94 @@ export class HomeComponent implements OnInit{
       this.httpData=data;
       this.array=this.httpData.menu;
     });
+    this.utterance = new SpeechSynthesisUtterance();
+
   }
-  ngOnInit(): void {
-    console.log(AccesibilidadComponent)
-    throw new Error('Method not implemented.');
+//accesibilidad
+  //Cambiar la fuente
+  texto = 'Texto de ejemplo';
+  selectedFont = 'Arial';
+  originalFont = 'Arial';
+  isAlternateFont = false;
+
+  data: any;
+  private utterance: SpeechSynthesisUtterance;
+
+
+  reanudarLectura() {
+    if (this.utterance) {
+      speechSynthesis.speak(this.utterance);
+    }
+  }
+
+
+
+  pausarLectura() {
+    if (this.utterance) {
+      speechSynthesis.pause();
+    }
+  }
+
+  reiniciarLectura() {
+    if (this.utterance) {
+      speechSynthesis.cancel();
+    }
+  }
+
+
+
+
+
+  ngOnInit() {
+
+    const contenido = document.getElementById('contenido')?.innerText;
+
+    if (contenido) {
+      this.utterance = new SpeechSynthesisUtterance(contenido);
+    }
+  }
+
+
+  cambiarFuente() {
+    if (this.isAlternateFont) {
+      this.selectedFont = this.originalFont;
+    } else {
+      this.selectedFont = 'Verdana';
+    }
+
+    this.isAlternateFont = !this.isAlternateFont;
+  }
+
+  // Escala de grises
+  public grayscale: boolean = false;
+
+  toggleGrayscale() {
+    this.grayscale = !this.grayscale;
+  }
+
+  // Tamaños de letra
+  public fontSize = 16; // Tamaño de letra inicial
+
+  increaseFontSize() {
+    this.fontSize += 2; // Incrementa el tamaño de letra en 2
+    //console.log(this.fontSize);
+  }
+
+  decreaseFontSize() {
+    if (this.fontSize > 2) {
+      this.fontSize -= 2; // Decrementa el tamaño de letra en 2 (con límite mínimo de 2)
+    }
+  }
+
+
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
   }
 
 }
