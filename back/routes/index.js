c10079524eafae694bfb604c65e8202be48f13db
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-06-17 11:45:24 -0600

    se implemento la api

--- back/routes/index.js
+++ back/routes/index.js
@@ -84,6 +84,33 @@ router.post('/mail', [
 
 });
 
+router.post('/mail/citas', [
+  body('user').not().isEmpty().isString(),
+  body('subject').not().isEmpty().isString(),
+  body('text').not().isEmpty().isString()
+], (req, res) => {
+  console.log("estoy enviando un correo :O0");
+  let dato = req.body
+  console.log(req.body.user)
+  transporter.sendMail({
+    from: '"Vittoria mails 👩‍💻👨‍💻👻" <peachycoold@gmail.com>', // sender address
+    to: dato.user, // list of receivers *----------------------aqui se colocaria el objeto recuperado que nos envia el formlario
+    subject: dato.subject,// "Hello ✔", // Subject line
+    text: dato.text, //"Hello world? esto funciona?", // plain text body
+    html: dato.text, // html body
+
+  }, (error, info) => {
+    console.log("Nueva Cita 👩‍💻👨‍💻👻");
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error al enviar el correo electrónico');
+    } else {
+      console.log('Correo enviado: ' + info.response);
+      res.status(200).send('Correo enviado correctamente');
+    }
+  });
+
+});
 
 
 module.exports = router;
