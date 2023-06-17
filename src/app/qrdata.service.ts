cd862b0dcf6ceee6a9acb5de12174ed7b925b8d5
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-06-16 18:39:38 -0600

    correcciones en la api

--- src/app/qrdata.service.ts
+++ src/app/qrdata.service.ts
@@ -9,10 +9,6 @@ export class QrdataService {
   constructor( private httpClient: HttpClient) {
   }
   reciveData(url:string ){
-<<<<<<< HEAD
     return this.httpClient.get(url);
-=======
-    return this.httpClient.get(url).toPromise()
->>>>>>> 7b4ba19b8c0648efa3ef7a86fe5704b2fa9df577
   }
 }
